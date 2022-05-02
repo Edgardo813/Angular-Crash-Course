9d1975eaf0b2ca36d209f8790e9ed7903f0ae7ce
Edgard <edgard.david179@gmail.com>
2022-05-02 18:11:22 -0400

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http'
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtomComponent } from './components/buttom/buttom.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtomComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AddTaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,31 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  
+  tasks: Task[] =[]
+
+  constructor(private TaskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.TaskService.getTasks().subscribe((tasks) => this.tasks = tasks);
+  }
+
+  deleteTasks(task: Task) {
+    this.TaskService.deleteTask(task).subscribe(() => (this.tasks = this.tasks.filter(t => t.id !== task.id)))
+  }
+
+  toggleReminder(task: Task ) {
+    task.reminder = !task.reminder;
+    this.TaskService.updateTaskReminder(task).subscribe();
+    
+  }
+
+}
